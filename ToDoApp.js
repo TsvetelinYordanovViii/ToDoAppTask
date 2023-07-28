c757b82373f25bdbb7626751e63d87255ed85239
Tsvetelin Yordanov <tsvetelin_yordanov_viii@abv.bg>
2023-07-28 11:30:24 +0300

    Added condition for checking whether there is something written in the to do list field. Now added and edited tasks will have any trailing and leading whitespaces trimmed.

--- ToDoApp.js
+++ ToDoApp.js
@@ -1,16 +1,17 @@
 let largestIndex = 1;
 
 addItem = (givenIndex, value) => {
-    //Initializing variables.
-    let itemIndex = largestIndex;
-    let itemClass = "item" + itemIndex;
-    let paragraph = $("<p></p>");
-    let newItem = $("<li></li>");
-    let content = $("<div></div>");
-    let options = $("<div></div>");
+    if (value != "") {
+        //Initializing variables.
+        let itemIndex = largestIndex;
+        let itemClass = "item" + itemIndex;
+        let paragraph = $("<p></p>");
+        let newItem = $("<li></li>");
+        let content = $("<div></div>");
+        let options = $("<div></div>");
 
-    //For the edit and delete button, I decided it will be better if I just inject the code directly here.
-    let editButton = $(`
+        //For the edit and delete button, I decided it will be better if I just inject the code directly here.
+        let editButton = $(`
     <button class="btn border  me-2 edit">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
             fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
@@ -20,9 +21,9 @@ addItem = (givenIndex, value) => {
             d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
         </svg>
     </button>`
-    );
+        );
 
-    let deleteButton = $(`
+        let deleteButton = $(`
     <button class="btn border  me-2 delete">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
             fill="currentColor" class="bi bi-trash2" viewBox="0 0 16 16">
@@ -30,53 +31,58 @@ addItem = (givenIndex, value) => {
             d="M14 3a.702.702 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671L2.037 3.225A.703.703 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2zM3.215 4.207l1.493 8.957a1 1 0 0 0 .986.836h4.612a1 1 0 0 0 .986-.836l1.493-8.957C11.69 4.689 9.954 5 8 5c-1.954 0-3.69-.311-4.785-.793z" />
         </svg>
     </button>`
-    );
+        );
 
-    //Adding the classes of all the elements in the To Do item.
-    content.addClass("col-8 d-flex align-items-center ms-1");
-    paragraph.addClass("align-items-center m-0");
-    //The value of the To Do item is also added here.
-    paragraph.text(value);
-    options.addClass("col-2 d-flex align-items-center justify-content-end visually-hidden taskOptions");
+        //Adding the classes of all the elements in the To Do item.
+        content.addClass("col-8 d-flex align-items-center ms-1");
+        paragraph.addClass("align-items-center m-0");
+        //The value of the To Do item is also added here.
+        value = value.trim()
+        paragraph.text(value);
+        options.addClass("col-2 d-flex align-items-center justify-content-end visually-hidden taskOptions");
 
-    newItem.addClass("ms-2 list-group-item d-flex flex-row justify-content-between");
+        newItem.addClass("ms-2 list-group-item d-flex flex-row justify-content-between");
 
-    //Check for an unused item id after existing items.
-    //Some dot shenanigans are at play here because for adding a class the dot is not required while for selecting by class it is.
-    if (givenIndex == -1) {
-        while ($("." + itemClass).length > 0) {
-            itemIndex++;
-            itemClass = "item" + itemIndex;
-        };
+        //Check for an unused item id after existing items.
+        //Some dot shenanigans are at play here because for adding a class the dot is not required while for selecting by class it is.
+        if (givenIndex == -1) {
+            while ($("." + itemClass).length > 0) {
+                itemIndex++;
+                itemClass = "item" + itemIndex;
+            };
+        }
+        else {
+            itemClass = givenIndex;
+        }
+        newItem.addClass(itemClass);
+
+        //Assembling the completed To Do item.
+        content.append(paragraph);
+        options.append(editButton, deleteButton);
+        newItem.append(content, options);
+        //Adding event listeners for hovering over the item. It is about hiding and revealing the option buttons.
+        //I can't use the toggle class method because the save or cancel function doesn't accordingly change the visibility 
+        //of the options box. If set to toggle, then cancelling or saving the editing of an item will cause the options box.
+        //to be hidden when the mouse hovers over it and be visible when the cursor is outside the task.
+        $(newItem).hover(() => {
+            $("." + itemClass + " .taskOptions").removeClass("visually-hidden");
+        }, () => {
+            $("." + itemClass + " .taskOptions").addClass("visually-hidden");
+        });
+
+        editButton.click(() => { editItem(itemClass) });
+        deleteButton.click(() => { deleteItem(itemClass) });
+
+        //The function must not save the items again when it loads them from local storage.
+        //-1 is given only when adding new items via tha add button.
+        if (givenIndex == -1)
+            localStorage.setItem(itemClass, value);
+
+        $("#toDoList").append(newItem);
     }
     else {
-        itemClass = givenIndex;
+        alert("You haven't written anything.");
     }
-    newItem.addClass(itemClass);
-
-    //Assembling the completed To Do item.
-    content.append(paragraph);
-    options.append(editButton, deleteButton);
-    newItem.append(content, options);
-    //Adding event listeners for hovering over the item. It is about hiding and revealing the option buttons.
-    //I can't use the toggle class method because the save or cancel function doesn't accordingly change the visibility 
-    //of the options box. If set to toggle, then cancelling or saving the editing of an item will cause the options box.
-    //to be hidden when the mouse hovers over it and be visible when the cursor is outside the task.
-    $(newItem).hover(() => {
-        $("." + itemClass + " .taskOptions").removeClass("visually-hidden");
-    }, () => {
-        $("." + itemClass + " .taskOptions").addClass("visually-hidden");
-    });
-
-    editButton.click(() => { editItem(itemClass) });
-    deleteButton.click(() => { deleteItem(itemClass) });
-
-    //The function must not save the items again when it loads them from local storage.
-    //-1 is given only when adding new items via tha add button.
-    if (givenIndex == -1)
-        localStorage.setItem(itemClass, value);
-
-    $("#toDoList").append(newItem);
 }
 
 deleteItem = (item) => {
@@ -134,8 +140,9 @@ saveOrCancelEdit = (itemClass, cancel) => {
     $("." + itemClass + " div:nth-child(3) .edit").removeClass("visually-hidden");
 
     if (!cancel) {
-        taskText.text(editingField.val());
-        localStorage.setItem(itemClass, editingField.val());
+        let value = editingField.val().trim()
+        taskText.text(value);
+        localStorage.setItem(itemClass, value);
     }
     editingField.remove();
     editedItem.removeClass("editing visually-hidden");
