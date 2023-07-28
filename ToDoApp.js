ec20c5c5cf716e1baa92360dbf75b933ec34ced9
Tsvetelin Yordanov <tsvetelin_yordanov_viii@abv.bg>
2023-07-28 13:27:29 +0300

    Missed the ToDoApp.js file during the comit.

--- ToDoApp.js
+++ ToDoApp.js
@@ -1,7 +1,7 @@
 let largestIndex = 1;
 
 addItem = (givenIndex, value) => {
-    if (value != "") {
+    if (value.trim() != "") {
         //Initializing variables.
         let itemIndex = largestIndex;
         let itemClass = "item" + itemIndex;
@@ -12,25 +12,25 @@ addItem = (givenIndex, value) => {
 
         //For the edit and delete button, I decided it will be better if I just inject the code directly here.
         let editButton = $(`
-    <button class="btn border  me-2 edit">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-            fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-            <path
-            d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-            <path fill-rule="evenodd"
-            d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-        </svg>
-    </button>`
+        <button class="btn border  me-2 edit">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                <path
+                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                <path fill-rule="evenodd"
+                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+            </svg>
+        </button>`
         );
 
         let deleteButton = $(`
-    <button class="btn border  me-2 delete">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-            fill="currentColor" class="bi bi-trash2" viewBox="0 0 16 16">
-            <path
-            d="M14 3a.702.702 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671L2.037 3.225A.703.703 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2zM3.215 4.207l1.493 8.957a1 1 0 0 0 .986.836h4.612a1 1 0 0 0 .986-.836l1.493-8.957C11.69 4.689 9.954 5 8 5c-1.954 0-3.69-.311-4.785-.793z" />
-        </svg>
-    </button>`
+        <button class="btn border  me-2 delete">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                fill="currentColor" class="bi bi-trash2" viewBox="0 0 16 16">
+                <path
+                d="M14 3a.702.702 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671L2.037 3.225A.703.703 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2zM3.215 4.207l1.493 8.957a1 1 0 0 0 .986.836h4.612a1 1 0 0 0 .986-.836l1.493-8.957C11.69 4.689 9.954 5 8 5c-1.954 0-3.69-.311-4.785-.793z" />
+            </svg>
+        </button>`
         );
 
         //Adding the classes of all the elements in the To Do item.
@@ -139,7 +139,7 @@ saveOrCancelEdit = (itemClass, cancel) => {
     $("." + itemClass + " div:nth-child(3) .cancel").remove();
     $("." + itemClass + " div:nth-child(3) .edit").removeClass("visually-hidden");
 
-    if (!cancel) {
+    if (!cancel && editingField.val().trim() != "") {
         let value = editingField.val().trim()
         taskText.text(value);
         localStorage.setItem(itemClass, value);
